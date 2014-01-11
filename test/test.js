cd38a853a5e635390f205c1851ef71be13625984
Arnaud <arnaud.courbiere@gmail.com>
2014-01-10 18:28:50 -0800

    Adding first real tests.

--- test/test.js
+++ test/test.js
@@ -1,10 +1,38 @@
 var assert = require('assert');
+var co = require('co');
+var mongo = require('..');
+var mongodb = require('mongodb');
 
-describe('Array', function () {
-    describe('#indexOf()', function () {
-        it('should return -1 when the value is not present', function () {
-            assert.equal(-1, [1,2,3].indexOf(5));
-            assert.equal(-1, [1,2,3].indexOf(0));
-        });
+describe('Getting the DB', function () {
+    it('should return the DB', function (done) {
+        co(function *() {
+            var db = yield mongo.getDb('mongodb://localhost:27017/test');
+            
+            assert.ok(db instanceof mongodb.Db);
+        })(done);
     });
 });
+
+describe('Geting a collection', function () {
+    it('should return the collection', function (done) {
+        co(function *() {
+            var db = yield mongo.getDb('mongodb://localhost:27017/test');
+            var collection = yield mongo.getCollection(db, 'testcollection');
+            
+            assert.ok(collection instanceof mongodb.Collection);
+        })(done);
+    });
+});
+
+describe('Getting a cursor as an array', function () {
+    it('should return a cursor as an array', function (done) {
+        co(function *() {
+            var db = yield mongo.getDb('mongodb://localhost:27017/test');
+            var collection = yield mongo.getCollection(db, 'testcollection');
+            var items = yield mongo.cursorToArray(collection.find());
+            
+            assert.ok(items instanceof Array);
+            assert.ok(items.length === 0);
+        })(done);
+    });
+})
