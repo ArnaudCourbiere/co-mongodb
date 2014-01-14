48f6b962193ceb9927fc410f5c9c09a5b6c60f9d
Arnaud <arnaud.courbiere@gmail.com>
2014-01-13 21:42:53 -0800

    Using better way to build manipulation functions.

--- README.md
+++ README.md
@@ -3,6 +3,26 @@ co-mongodb
 
 [![Build Status](https://travis-ci.org/ArnaudCourbiere/co-mongodb.png?branch=master)](https://travis-ci.org/ArnaudCourbiere/co-mongodb)
 
-Manipulation library to use the mongodb native driver with generator based flow control libraries such as co.
+Manipulation library to use the mongodb native driver with generator based flow control libraries such as [co](https://github.com/visionmedia/co).
 
-Examples to come soon...
+## Instalation
+
+```
+$ npm install co-mongodb
+```
+## Example
+
+```js
+var comongo = require('co-mongodb');
+
+co(function *() {
+  db = yield comongo.client.connect('mongodb://localhost:27017/test');
+  var collection = yield comongo.db.collection(db, 'testCollection');
+  
+  var result = yield comongo.db.addUser(db, 'user', 'pass');
+  var user = result[0];
+  yield comongo.db.removeUser(db, 'user');
+  
+  yield comongo.db.close(db);
+})();
+```
--- test/db.js
+++ test/db.js
@@ -12,10 +12,12 @@ describe('db', function () {
     })(done);
   });
   afterEach(function (done) {
-    db.close(done);
-    db = undefined;
+    db.dropDatabase(function (err, result) {
+      db.close(done);
+      db = undefined;
+    });
   });
-  
+
   describe('#open()', function () {
     it('should initialize the database connection', function (done) {
       co(function *() {
@@ -69,7 +71,7 @@ describe('db', function () {
   describe('#collection()', function () {
     it('should fetch a collection', function (done) {
       co(function *() {
-        var collection = yield comongo.db.collection(db, 'testCollection');
+        var collection = yield comongo.db.collection(db, 'testCollection1');
         assert.ok(collection instanceof mongodb.Collection);
       })(done);
     });
@@ -153,14 +155,174 @@ describe('db', function () {
   describe('#createCollection()', function () {
     it('should create a collection on the server', function (done) {
       co(function *() {
-        var collection = yield comongo.db.createCollection(db, 'testCollection');
+        var collection = yield comongo.db.createCollection(db, 'testCollection2');
         assert.ok(collection instanceof mongodb.Collection);
-        db.dropCollection('testCollection', function (err, result) {
-          assert.strictEqual(err, null);
-          assert.strictEqual(result, true);
-          done();
+      })(done);
+    });
+  });
+  
+  describe('#command()', function () {
+    it('should execute a command hash against MongoDB', function (done) {
+      co(function *() {
+        var result = yield comongo.db.command(db, { ping: 1 });
+        assert.strictEqual(result.ok, 1);
+      })(done);
+    });
+  });
+  
+  describe('#dropCollection()', function () {
+    it('should drop the collection on the server', function (done) {
+      co(function *() {
+        yield comongo.db.createCollection(db, 'testCollection3');
+        var result = yield comongo.db.dropCollection(db, 'testCollection3');
+        assert.strictEqual(result, true);
+      })(done);
+    });
+  });
+  
+  describe('#renameCollection()', function () {
+    it('should rename the collection', function (done) {
+      co(function *() {
+        yield comongo.db.createCollection(db, 'testCollection4');
+        var collection = yield comongo.db.renameCollection(db, 'testCollection4', 'testCollection4Renamed');
+        assert.ok(collection instanceof mongodb.Collection);
+        assert.strictEqual(collection.collectionName, 'testCollection4Renamed');
+      })(done);
+    });
+  });
+  
+  describe('#lastError()', function () {
+    it('should return the last error message from the given connection', function (done) {
+      co(function *() {
+        var result = yield comongo.db.lastError(db);
+        assert.strictEqual(result[0].err, null);
+        assert.strictEqual(result[0].ok, 1);
+      })(done);
+    });
+  });
+  
+  describe('#previousErrors()', function () {
+    it('should return all errors up to the last time db reset_error_history was called', function (done) {
+      co(function *() {
+        var result = yield comongo.db.previousErrors(db);
+        assert.strictEqual(result[0].err, null);
+        assert.strictEqual(result[0].ok, 1);
+      })(done);
+    });
+  });
+  
+  describe('#resetErrorHistory()', function () {
+    it('should the error history of the mongo instance', function (done) {
+      co(function *() {
+        var result = yield comongo.db.resetErrorHistory(db);
+        assert.strictEqual(result[0].ok, 1);
+      })(done);
+    });
+  });
+  
+  describe('#createIndex()', function () {
+    it('should create an index on the collection', function (done) {
+      db.createCollection('testCollection5', function(err, collection) {
+        assert.equal(null, err);
+
+        collection.insert([{a:1, b:1}, {a:1, b:1}, {a:2, b:2}, {a:3, b:3},
+          {a:4, b:4}], {w:1}, function(err, result) {
+            
+          co(function *() {
+            var result = yield comongo.db.createIndex(db, 'testCollection5', {a:1, b:1},
+              {unique:true, background:true, dropDups:true, w:1});
+            
+            assert.strictEqual(result, 'a_1_b_1');
+          })(done);
         });
-      })();
+      });
+    });
+  });
+  
+  describe('#ensureIndex()', function () {
+    it('should create an index on the collection', function (done) {
+      db.createCollection('testCollection6', function(err, collection) {
+        assert.equal(null, err);
+
+        collection.insert([{a:1, b:1}, {a:1, b:1}, {a:2, b:2}, {a:3, b:3},
+          {a:4, b:4}], {w:1}, function(err, result) {
+            
+          co(function *() {
+            var result = yield comongo.db.ensureIndex(db, 'testCollection6', {a:1, b:1},
+              {unique:true, background:true, dropDups:true, w:1});
+            
+            assert.strictEqual(result, 'a_1_b_1');
+          })(done);
+        });
+      });
+    });
+  });
+  
+  describe('#cursorInfo()', function () {
+    it('should return the cursor information', function (done) {
+      co(function *() {
+        var cursorInfo = yield comongo.db.cursorInfo(db);
+        assert.strictEqual(cursorInfo.ok, 1);
+        assert.strictEqual(cursorInfo.totalOpen, 0);
+        assert.strictEqual(cursorInfo.timedOut, 0);
+      })(done);
+    });
+  });
+  
+  describe('#dropIndex()', function () {
+    it('should drop an index on a collection', function (done) {
+      co(function *() {
+        var collection = yield comongo.db.createCollection(db, 'testCollection7');
+        yield comongo.db.ensureIndex(db, 'testCollection7', {a:1, b:1},
+          {unique:true, background:true, dropDups:true, w:1});
+        var result = yield comongo.db.dropIndex(db, 'testCollection7', 'a_1_b_1');
+        assert.strictEqual(result.nIndexesWas, 2);
+        assert.strictEqual(result.ok, 1);
+      })(done);
+    });
+  });
+  
+  describe('#reIndex()', function () {
+    it('should reindex all indexes on a collection', function (done) {
+      co(function *() {
+        var collection = yield comongo.db.createCollection(db, 'testCollection8');
+        yield comongo.db.ensureIndex(db, 'testCollection8', {a:1, b:1},
+          {unique:true, background:true, dropDups:true, w:1});
+        
+        var result = yield comongo.db.reIndex(db, 'testCollection8');
+        assert.strictEqual(result, true);
+      })(done);
+    });
+  });
+  
+  describe('#indexInformation()', function () {
+    it('should return a collection index info', function (done) {
+      co(function *() {
+        var collection = yield comongo.db.createCollection(db, 'testCollection9');
+        yield comongo.db.ensureIndex(db, 'testCollection8', {a:1, b:1},
+          {unique:true, background:true, dropDups:true, w:1});
+        
+        var indexInfo = yield comongo.db.indexInformation(db, 'testCollection9');
+        assert.deepEqual([ [ '_id', 1 ] ], indexInfo._id_);
+      })(done);
+    });
+  });
+  
+  describe('#dropDatabase()', function () {
+    it('should drop the database', function (done) {
+      co(function *() {
+        var result = yield comongo.db.dropDatabase(db);
+        assert.strictEqual(result, true);
+      })(done);
+    });
+  });
+  
+  describe('#stats()', function () {
+    it('should get all database statistics', function (done) {
+      co(function *() {
+        var stats = yield comongo.db.stats(db);
+        assert.strictEqual(stats.db, 'test');
+      })(done);
     });
   });
 });
